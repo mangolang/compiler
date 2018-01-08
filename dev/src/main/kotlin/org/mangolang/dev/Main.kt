67c8c36600409f978010eed35f9a5bc86ae6883b
mark <mark@rafiki>
2018-01-08 22:33:43 +0100

    add most of the modules that seem to be necessary, as well as their mutual includes

---
+++ dev/src/main/kotlin/org/mangolang/dev/Main.kt
@@ -0,0 +1,8 @@
+package org.mangolang.dev
+
+fun main(arg: Array<String>) {
+    // For development testing, bypass the command line interface and tell the conductor to do things.
+    println("Hello, world!")
+}
+
+
