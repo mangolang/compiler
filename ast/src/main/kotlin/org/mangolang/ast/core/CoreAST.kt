adf3008b14681b672c823a36f92106ec3abdd9b0
mark <mark@rafiki>
2018-01-08 22:12:03 +0100

    abstract syntax tree: two versions for before and after reduction

---
+++ ast/src/main/kotlin/org/mangolang/ast/core/CoreAST.kt
@@ -0,0 +1,5 @@
+package org.mangolang.ast.core
+
+interface CoreAST {
+}
+
---
+++ ast/src/main/kotlin/org/mangolang/ast/dual/DualAST.kt
@@ -0,0 +1,8 @@
+package org.mangolang.ast.dual
+
+import org.mangolang.ast.core.CoreAST
+import org.mangolang.ast.full.FullAST
+
+interface DualAST: CoreAST, FullAST {
+}
+
