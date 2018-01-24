0c8f36fb74df9ff7b5e50860e1af130c9acb698a
mark <mark@arrow>
2018-01-24 19:08:21 +0100

    split AST into full and core

--- ast/src/main/kotlin/org/mangolang/ast/full/FullAST.kt
+++
@@ -1,5 +0,0 @@
-package org.mangolang.ast.full
-
-interface FullAST {
-}
-
---
+++ coreast/src/main/kotlin/org/mangolang/coreast/CoreAST.kt
@@ -0,0 +1,11 @@
+package org.mangolang.coreast
+
+import org.mangolang.coreast.CoreAST
+
+/**
+ * An element in the reduced abstract syntax tree of the language, with 'synonyms' removed.
+ * Produced from [FullAST] by the reducer.
+ */
+interface CoreAST {
+}
+
