5b3ccc7eab56680fb8bde5c9b29689f4a230ae5a
mark <mark@arrow>
2018-01-24 19:22:32 +0100

    create the interface types for the different levels of representation

---
+++ sem/src/main/kotlin/org/mangolang/sem/Sem.kt
@@ -0,0 +1,9 @@
+package org.mangolang.sem
+
+/**
+ * This is an element in the abstract syntax tree after all the variables
+ * and references have been connected in their scope.
+ */
+class Sem {
+}
+
