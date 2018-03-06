ff8b80d37647de1851a647e97ed3758c83c26c4e
Mark <mverleg.noreply@gmail.com>
2018-03-06 22:03:12 +0100

    Create AST nodes for assignments

---
+++ fullast/src/main/kotlin/org/mangolang/fullast/BindingAST.kt
@@ -0,0 +1,23 @@
+
+/* Mango compiler (mangolang.org) | Apache 2.0 license, © 2018. */
+
+package org.mangolang.fullast
+
+/**
+ * Type for an association, e.g. assignment.
+ */
+interface BindingAST : ExpressionAST
+
+/**
+ * Implement a operation on two targets.
+ */
+data class Assignment(
+        val assignee: ExpressionAST,
+        val value: ExpressionAST) : BindingAST {
+
+    /**
+     * {@see HasTextRepresentation.asText()}.
+     */
+    override fun asText(): CharSequence =
+            "${assignee.asText()} = ${value.asText()}"
+}
