6b72ff1ccaf17ed9ada8c36f1bba8729eb332dce
Mark <mverleg.noreply@gmail.com>
2018-03-10 17:20:54 +0100

    Add module files to all projects

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+mod mango {
+    // Utilities
+    mod util;
+    mod cli;
+    mod jit;
+
+    // Types
+    mod token;
+    mod ast_core;
+    mod ast_full;
+    mod sem;
+    mod ir;
+
+    // Operations
+    mod lexing;
+    mod parsing;
+    mod reducing;
+    mod semanticating;
+    mod typing;
+    mod optimizing;
+}
+
+// For development
+mod playground;
