5be332e59213226bc41e4565e4b1fe54cf9205f0
Mark <mverleg.noreply@gmail.com>
2018-03-12 22:39:55 +0100

    Full ast module layout

--- src/mango/ast_full/mod.rs
+++ src/mango/ast_full/mod.rs
@@ -1,11 +1,8 @@
-//
-// todo: remove
-//pub struct FASTTest {
-//    p: i32,
-//    r: i32
-//}
-//impl FASTTest {
-//    pub fn new() -> FASTTest {
-//        FASTTest { p: 1, r: 2 }
-//    }
-//}
+mod node;
+pub use self::node::*;
+
+mod terminal;
+pub use self::terminal::*;
+
+mod collect;
+pub use self::collect::FullAST;
