b61d6f2d61f3f5281303d105b3f69e9af569a506
Mark <mverleg.noreply@gmail.com>
2018-06-17 20:53:45 +0200

    Parsing floats implemented #52

--- src/mango/util/parsetxt/int.rs
+++ src/mango/util/parsetxt/int.rs
@@ -12,7 +12,7 @@ pub enum IntParseFailReason {
 /// The syntax for -37 in base 16 is -16b25 and 2748 is 16bABC.
 /// Incorrect values like 4b7 or 0b0 are not handled at the lexing stage.
 pub fn int_pattern() -> &'static str {
-    r"(?P<sign>\+|-?)(?:(?P<base>[1-9][0-9]*)b(?P<reb_val>(?:_?[0-9a-zA-Z])+)|(?P<b10_val>[0-9](?:_?[0-9])*))"
+    r"(?:(?P<base>(?:\+|-?)[1-9][0-9]*)b(?P<reb_val>(?:_?[0-9a-zA-Z])+)|(?P<b10_val>(?:\+|-?)[0-9](?:_?[0-9])*))"
 }
 
 /// Convert a String that matches [int_pattern] to an i64 integer. Overflow is possible.
@@ -24,13 +24,13 @@ pub fn parse_int<S: Into<String>>(text: S) -> Result<i64, IntParseFailReason> {
     {
         None => return Err(IntParseFailReason::Invalid),
         Some(captures) => {
-            // Sign
-            let sign_str = captures.name("sign").unwrap().as_str();
-            let sign = if sign_str == "+" || sign_str == "" {
-                1 // positive
-            } else {
-                -1 // negative
-            };
+            //            // Sign
+            //            let sign_str = captures.name("sign").unwrap().as_str();
+            //            let sign = if sign_str == "+" || sign_str == "" {
+            //                1 // positive
+            //            } else {
+            //                -1 // negative
+            //            };
             // Check if base10 or special
             match captures.name("b10_val") {
                 None => {
@@ -61,16 +61,32 @@ pub fn parse_int<S: Into<String>>(text: S) -> Result<i64, IntParseFailReason> {
 }
 
 // TODO: possibly add a i32 version?
-// TODO: Option<i64> to deal with overflow?
 
 #[cfg(test)]
 mod tests {
     use super::parse_int;
 
     #[test]
-    fn test_parse_int() {
+    fn test_parse_b10_int() {
         assert_eq!(42, parse_int("42").unwrap());
         assert_eq!(42, parse_int("4_2").unwrap());
-        // assert_eq!(42, parse_int("10b4_2").unwrap());
+        assert_eq!(123456789, parse_int("+1_2_3_4_5_6_7_8_9").unwrap());
+        assert_eq!(-123456789, parse_int("-123456789").unwrap());
+        assert_eq!(0, parse_int("-0").unwrap());
+        assert_eq!(-1, parse_int("-1").unwrap());
+        // Weird bases with 0 prefix are not supported.
+        assert_eq!(9, parse_int("09").unwrap());
+    }
+
+    fn test_invalid_b10_ints() {
+        assert!(parse_int("0x9").is_err());
+        assert!(parse_int("A").is_err());
+        assert!(parse_int("_0").is_err());
+        assert!(parse_int("0_").is_err());
+        // TODO: over/underflow
+    }
+
+    fn test_parse_based_ints() {
+        // TODO: not implemented yet
     }
 }
--- src/mango/util/parsetxt/real.rs
+++ src/mango/util/parsetxt/real.rs
@@ -1,3 +1,4 @@
+use mango::util::strslice::char_ops::char_drop;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -13,32 +14,93 @@ pub enum RealParseFailReason {
 /// Here int is a series of 0-9 digits separated by at most one underscore.
 /// Signs are optional, everything from 'e' is optional, and int1 OR int2 is optional.
 pub fn real_pattern() -> &'static str {
-    // TODO: do I want to allow numbers to start with a period?
+    // TODO: do I really want to allow numbers to start with a period?
     // TODO: for now, only base10 for reals (would 8b11e2 be 9*8^2 or 9*10^2?)
     // TODO: does not deal with NaN of infinity
-    r"(?P<sign>\+|-?)(?:\d(?:_?\d)*\.\d(?:_?\d)*|\d(?:_?\d)*\.|\.\d(?:_?\d)*)(?:e(?P<exp_sign>\+|-?)\d(?:_?\d)*)?"
+    r"(?P<multiplier>(?:\+|-?)(?:\d(?:_?\d)*\.\d(?:_?\d)*|\d(?:_?\d)*\.|\.\d(?:_?\d)*))(?:e(?P<exponent>(?:\+|-?)\d(?:_?\d)*))?"
 }
 
 /// Convert a String that matches [real_pattern] to an f64 real. Overflow and loss of precision is possible.
 pub fn parse_real<S: Into<String>>(text: S) -> Result<f64, RealParseFailReason> {
     let text = text.into();
-    debug_assert!(
-        Regex::new(&format!("^{}$", real_pattern()))
-            .unwrap()
-            .is_match(&text)
-    );
-    Ok(0.0f64)
+    match Regex::new(&format!("^{}$", real_pattern()))
+        .unwrap()
+        .captures(&text)
+    {
+        None => return Err(RealParseFailReason::Invalid),
+        Some(captures) => {
+            let multiplier = char_drop(captures.name("multiplier").unwrap().as_str(), &'_')
+                .parse::<f64>()
+                .unwrap();
+            match captures.name("exponent") {
+                None => {
+                    // This is a 'normal' real, no exponential notation
+                    return Ok(multiplier);
+                }
+                Some(exponent_match) => {
+                    // This real is in exponential notation
+                    let exponent = char_drop(exponent_match.as_str(), &'_')
+                        .parse::<f64>()
+                        .unwrap();
+                    // TODO: is there a numerically smarter way to do this?
+                    return Ok(10f64.powf(exponent) * multiplier);
+                }
+            }
+        }
+    }
 }
 
-// TODO: possibly add a i32 version?
-// TODO: Option<i64> to deal with overflow?
+// TODO: possibly add a f32 version?
 
 #[cfg(test)]
 mod tests {
     use super::parse_real;
 
-    #[test]
-    fn test_parse_real() {
-        assert_eq!(42., parse_real("42.").unwrap())
+    fn close(x: f64, y: f64) -> bool {
+        (x - y).abs() < 1e-8
     }
+
+    #[test]
+    fn test_parse_nonexp_real() {
+        assert!(close(42., parse_real("42.0").unwrap()));
+        assert!(close(-0.1, parse_real("-.1").unwrap()));
+        assert!(close(-1., parse_real("-1.").unwrap()));
+        assert!(close(12345.6789, parse_real("1_2_3_4_5.6_7_8_9").unwrap()));
+    }
+
+    #[test]
+    fn test_parse_exp_real() {
+        assert!(close(42., parse_real("42.0e0").unwrap()));
+        assert!(close(-0.1, parse_real("-.1e0").unwrap()));
+        assert!(close(-1., parse_real("-1.e0").unwrap()));
+        assert!(close(42., parse_real("42.0e+0").unwrap()));
+        assert!(close(
+            12345.6789,
+            parse_real("1_2_3_4_5.6_7_8_9e0").unwrap()
+        ));
+        assert!(close(0.42, parse_real("42.0e-2").unwrap()));
+        assert!(close(-0.001, parse_real("-.1e-2").unwrap()));
+        assert!(close(-0.01, parse_real("-1.e-2").unwrap()));
+        assert!(close(
+            123.456789,
+            parse_real("1_2_3_4_5.6_7_8_9e-2").unwrap()
+        ));
+        assert!(close(42.0, parse_real("42.0e-0_0_0").unwrap()));
+    }
+
+    #[test]
+    fn test_invalid_real() {
+        assert!(parse_real("+_42.0").is_err());
+        assert!(parse_real("-_42.0").is_err());
+        assert!(parse_real("_42.0").is_err());
+        assert!(parse_real("42_.0").is_err());
+        assert!(parse_real("42._0").is_err());
+        assert!(parse_real("42.0_").is_err());
+        assert!(parse_real("42.0e_0").is_err());
+        assert!(parse_real("42.0e0_").is_err());
+        assert!(parse_real("42.0e0b0").is_err());
+    }
+
+    // TODO: over/underflow
+    // TODO: loss of precision
 }
