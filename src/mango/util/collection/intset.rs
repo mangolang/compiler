5b132c88914d2b944b858c68da22713ca7de8d7c
Mark <mverleg.noreply@gmail.com>
2018-07-02 00:45:47 +0200

    Add tests for integer set

--- src/mango/util/collection/intset.rs
+++ src/mango/util/collection/intset.rs
@@ -74,5 +74,71 @@ impl<T: Mod + Div> IntSet<T> {
             Bin::Empty => false,
         }
     }
+
+    /// Gives the number of elements in the set.
+    /// This actively counts the elements, so it is not free.
+    pub fn count(&self) -> usize {
+        let mut sum = 0;
+        for bin in self.bins {
+            sum += match bin {
+                Bin::SubSet(set) => set.count(),
+                Bin::Exists(_) => 1,
+                Bin::Empty => 0,
+            }
+        }
+        return sum;
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::IntSet;
+
+    #[test]
+    fn test_int_set_basic() {
+        let mut set = IntSet::new();
+        for k in 0 .. 128 {
+            set.insert(k);
+        }
+        for k in 0 .. 128 {
+            assert!(set.contains(k));
+        }
+        for k in -1024 .. 0 {
+            assert!(!set.contains(k));
+        }
+        for k in 128 .. 1024 {
+            assert!(!set.contains(k));
+        }
+        assert_eq!(set.count(), 128);
+    }
+
+    #[test]
+    fn test_int_set_repeats() {
+        let mut set = IntSet::new();
+        for k in 0 .. 128 {
+            set.insert(k);
+        }
+        for k in 0 .. 128 {
+            assert!(!set.insert(k));
+        }
+        assert_eq!(set.count(), 128);
+    }
+
+    #[test]
+    fn test_int_set_collisions() {
+        let mut set = IntSet::new();
+        // To have a value in the first bin for 6 levels, the last 24 bits should be 0.
+        // To let all the lower levels be created, add at least 6 values.
+        for k in 1 .. 9 {
+            assert!(set.insert(k * 2**24));
+        }
+        for k in 1 .. 9 {
+            assert!(set.contains(k * 2**24));
+        }
+        for k in 0 .. 128 {
+            assert!(!set.contains(k));
+        }
+        assert!(!set.contains(2**23));
+        assert_eq!(set.count(), 8);
+    }
+}
