91e189972e4368027f06347c04c49503f36739c5
Mark <mverleg.noreply@gmail.com>
2019-06-13 08:18:19 +0200

    Change return type of string parser

--- src/pestparser/parser.rs
+++ src/pestparser/parser.rs
@@ -1,17 +1,18 @@
 use ::pest::Parser;
 //use crate::pestparser::Error;
-use pest::error::Error;
+use ::pest::error::Error;
 use ::pest::iterators::Pairs;
+use ::pest::iterators::Tokens;
 
 #[derive(Parser)]
 #[grammar = "pestparser/mango.pest"]
 pub struct MangoParser;
 
-pub fn parse_string(source: &str) -> Result<Pairs<Rule>, Error<Rule>> {
-    MangoParser::parse(Rule::field, source)
+pub fn parse_string(source: &str) -> Result<Tokens<Rule>, Error<Rule>> {
+    Ok(MangoParser::parse(Rule::number, source)?.tokens())
 }
 
 #[test]
 fn test_parse_string() {
-    MangoParser::parse(Rule::field, "-3.1415");
+    assert_eq!(Rule::number, parse_string("-3.1415").unwrap());
 }
