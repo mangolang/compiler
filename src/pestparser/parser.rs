47fece2922278ce312c25cc51bfa4cbfed74a863
Mark <mverleg.noreply@gmail.com>
2020-06-07 23:36:27 +0200

    Some progress, but pest output is not close enough to AST

--- src/pestparser/parser.rs
+++ src/pestparser/parser.rs
@@ -10,14 +10,14 @@ use ::pest_derive::Parser;
 pub struct MangoParser;
 
 pub fn parse_string(source: &str) -> Result<Tokens<Rule>, Error<Rule>> {
-    dbg!(MangoParser::parse(Rule::number, source)?);
+    dbg!(MangoParser::parse(Rule::file, source)?.next().unwrap().into_inner());
     unimplemented!()
     //Ok(MangoParser::parse(Rule::number, source)?.next().into_inner())
 }
 
 #[test]
 fn test_parse_string() {
-    parse_string("-3.1415\n42\n\n");
+    parse_string("-3.1415\n+42\n\n");
     // assert_eq!(Rule::file, parse_string("-3.1415").unwrap());
     //unimplemented!()
 }
