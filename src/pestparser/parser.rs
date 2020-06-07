92dbdada22e4fecb630526ebb0aa28ca2dc20bb1
Mark <mverleg.noreply@gmail.com>
2020-06-07 23:05:10 +0200

    Parsing numbers with PEST

--- src/pestparser/parser.rs
+++ src/pestparser/parser.rs
@@ -2,17 +2,22 @@ use ::pest::Parser;
 //use crate::pestparser::Error;
 use ::pest::error::Error;
 use ::pest::iterators::Tokens;
+use ::pest_derive::Parser;
+
 
 #[derive(Parser)]
 #[grammar = "pestparser/mango.pest"]
 pub struct MangoParser;
 
 pub fn parse_string(source: &str) -> Result<Tokens<Rule>, Error<Rule>> {
-    Ok(MangoParser::parse(Rule::number, source)?.tokens())
+    dbg!(MangoParser::parse(Rule::number, source)?);
+    unimplemented!()
+    //Ok(MangoParser::parse(Rule::number, source)?.next().into_inner())
 }
 
 #[test]
 fn test_parse_string() {
-    // assert_eq!(Rule::number, parse_string("-3.1415").unwrap());
-    unimplemented!()
+    parse_string("-3.1415\n42\n\n");
+    // assert_eq!(Rule::file, parse_string("-3.1415").unwrap());
+    //unimplemented!()
 }
