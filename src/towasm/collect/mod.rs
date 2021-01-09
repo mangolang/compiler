67dc496e36f0699855d832a89b0923a4d87aa17b
Mark <mverleg.noreply@gmail.com>
2021-01-09 22:59:36 +0100

    Move wasm-related code to wasm repository

--- src/towasm/collect/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use self::datatype::Type;
-pub use self::statement::Statement;
-
-pub mod datatype;
-pub mod statement;
--- src/towasm/scope/mod.rs
+++
@@ -1,8 +0,0 @@
-pub use self::function::Function;
-pub use self::function::FunctionSignature;
-pub use self::function::Output;
-pub use self::function::Parameter;
-pub use self::module::Module;
-
-pub mod function;
-pub mod module;
